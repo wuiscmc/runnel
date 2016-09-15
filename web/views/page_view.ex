403bae7827e76de91078f4ebef53a34198d7a63a
Carlos Mateos <carlos.mateos@mynewsdesk.com>
2016-09-16 00:36:39 +0200

    Adds grouping by month helper in page_view

--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -1,3 +1,13 @@
 defmodule Runnel.PageView do
   use Runnel.Web, :view
+
+  defp group_runs_by_month(runs) do
+    Enum.group_by(runs, fn(run) ->
+      {{year, month, _}, _} = Ecto.DateTime.to_erl(run.start_time)
+      {year, month}
+    end)
+    |> Enum.reverse
+  end
+
+  defp group_runs_by_month([]), do: []
 end
