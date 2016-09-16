58a98c0a20f9a6473cbc69c51cfca4291649efdf
Carlos Mateos <carlos.mateos@mynewsdesk.com>
2016-09-16 21:31:19 +0200

    Fixes the “fetching all runs” bug

--- web/models/nike_service.ex
+++ web/models/nike_service.ex
@@ -1,63 +1,63 @@
 defmodule Runnel.NikeService do
   require Logger
   import Ecto.Query, only: [from: 2]
+  alias Runnel.NikeRun
 
   def fetch_new_runs(token) do
     Logger.debug "extract_new_runs"
 
-    new_runs = MapSet.difference(fetch_latest_runs(token), fetch_local_runs)
-
-    Enum.each(new_runs, fn(run) ->
+    token
+    |> fetch_latest_runs
+    |> Enum.each(fn(run) ->
      {:ok, _pid} = Task.Supervisor.start_child(Runnel.NikeRunTaskSupervisor, __MODULE__, :import_run, [token, run])
     end)
   end
 
-  def import_run(token, data) do
-    fetch_run_data(token, data)
-    |> save_run_data
+  def import_run(token, run_id) do
+    run = run_id
+          |> fetch_run_data(token)
+          |> (&NikeRun.changeset(%NikeRun{}, &1)).()
+
+    case Runnel.Repo.insert(run) do
+        {:error, _} -> Logger.error("could not insert run: #{run_id}")
+        {:ok, run}  -> run
+    end
   end
 
   defp fetch_latest_runs(token) do
     Logger.debug "fetch_latest_runs"
-    query = from run in Runnel.NikeRun, select: run.start_time, limit: 1, order_by: [desc: :start_time]
-    case Runnel.Repo.one(query) do
+
+    query = from run in NikeRun, select: run.start_time, limit: 1, order_by: [desc: :start_time]
+
+    activities = case Runnel.Repo.one(query) do
       nil ->
-        activities = Runnel.Integrations.NikeRuns.fetch_activity_list(token, count: 20)
-        Enum.map(activities["data"], fn(activity) -> activity["activityId"] end)
-        |> MapSet.new
+        Runnel.Integrations.NikeRuns.fetch_activity_list(token, count: 20)
 
       %Ecto.DateTime{} = datetime ->
         Logger.debug "fetching from #{datetime}"
 
         start_date = datetime
-                    |> Ecto.DateTime.to_date
-                    |> Ecto.Date.to_string
+                      |> Ecto.DateTime.to_date
+                      |> Ecto.Date.to_string
 
         end_date = :calendar.local_time
                     |> Ecto.DateTime.from_erl
                     |> Ecto.DateTime.to_date
                     |> Ecto.Date.to_string
 
-        activities = Runnel.Integrations.NikeRuns.fetch_activity_list(token, count: 20, startTime: start_date, endTime: end_date)
-        Enum.map(activities["data"], fn(activity) -> activity["activityId"] end)
-        |> MapSet.new
+        Runnel.Integrations.NikeRuns.fetch_activity_list(token, count: 20, startDate: start_date, endDate: end_date)
     end
 
+    Enum.map(activities["data"], fn(activity) -> activity["activityId"] end)
   end
 
-  defp fetch_local_runs do
-    Logger.debug "fetch_local_runs"
-
-    Runnel.Repo.all(from run in Runnel.NikeRun, select: run.activity_id)
-    |> MapSet.new
-  end
-
-  defp fetch_run_data(token, run_id) do
+  defp fetch_run_data(run_id, token) do
     Logger.debug "fetch_run_data for #{run_id}"
+
     run_data = Runnel.Integrations.NikeRuns.fetch_activity(token, run_id, gps: false)
     gps_coordinates = fetch_run_gps_data(token, run_id, run_data["isGpsActivity"])
 
-     %{
+    %{
       calories: run_data["metricSummary"]["calories"],
       duration: run_data["metricSummary"]["duration"],
       distance: run_data["metricSummary"]["distance"],
@@ -68,24 +68,14 @@ defmodule Runnel.NikeService do
     }
   end
 
-  defp save_run_data(run_data) do
-    run_data
-    |> (&Runnel.NikeRun.changeset(%Runnel.NikeRun{}, &1)).()
-    |> Runnel.Repo.insert!
-  end
-
   def fetch_run_gps_data(token, run_id, true) do
     response = Runnel.Integrations.NikeRuns.fetch_activity(token, run_id, gps: true)
 
     case response do
       %{"error_id" => _, "errors" => _} -> []
-      _ ->
-        Enum.map(response["waypoints"], fn(waypoint) ->
-          %{
-            "lng" => waypoint["longitude"],
-            "lat" => waypoint["latitude"]
-          }
-        end)
+      %{"waypoints" => waypoints} ->
+        waypoints
+        |> Enum.map(fn(waypoint) -> %{ "lng" => waypoint["longitude"], "lat" => waypoint["latitude"] } end)
     end
 
   end
